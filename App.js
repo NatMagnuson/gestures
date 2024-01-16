4ed88558505c2f53e5fcbc34e4db26dbb608b2d6
Nathaniel Magnuson <nathaniel.magnuson@lethbridgecollege.ca>
2024-01-16 09:11:21 -0700

    Added Gesture Handler WIP

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+    GestureHandlerRootView,
+    Gesture,
+    GestureDectector
+} from 'react-native-gesture-handler';
+import {
+    useSharedValue,
+    withTiming,
+    useAnimatedStyle,
+} from 'react-native-reanimated';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
