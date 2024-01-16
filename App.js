4d1f741ad4d9cc476e9685d0983b2ceb45dd6929
Nathaniel Magnuson <nathaniel.magnuson@lethbridgecollege.ca>
2024-01-16 09:36:03 -0700

    Continued build, having issue with function not being defined

--- App.js
+++ App.js
@@ -5,18 +5,48 @@ import {
     Gesture,
     GestureDectector
 } from 'react-native-gesture-handler';
-import {
+import Animated, {
     useSharedValue,
     withTiming,
     useAnimatedStyle,
 } from 'react-native-reanimated';
 
+const END_POSITION = 200;
+
 export default function App() {
-  return (
-    <GestureHandlerRootView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </GestureHandlerRootView>
+    const onLeft = useSharedValue(true);
+    const positionX = useSharedValue(0);
+
+    const panGesture = Gesture.Pan()
+        .OnUpdate((e) => {
+            if (onLeft.value) {
+                positionX.value = e.translationX;
+            } else {
+                positionX.value = END_POSITION + e.translationX;
+            }
+        })
+        .onEnd((e) => {  //snaps to one side or the other
+            if (positionX.value > END_POSITION / 2) {
+                positionX.value = withTiming(END_POSITION, { duration: 100 });
+                onLeft.value = false;
+            } else {
+                positionX.value = withTiming(0, { duration: 100 });
+                onLeft.value = true;
+            }
+        })
+
+    const animatedStyle = useAnimatedStyle(() => ({
+        transform: [{ translateX: positionX.value }],
+    }))
+
+    return (
+        <GestureHandlerRootView style={styles.container}>
+            <GestureDectector gesture={panGesture}>
+                <Animated.View style={[styles.box], animatedStyle } />
+            </GestureDectector>
+          <Text>Open up App.js to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </GestureHandlerRootView>
   );
 }
 
@@ -26,5 +56,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    },
+    box: {
+        height: 120,
+        width: 120,
+        backgroundColor: 'blue',
+        borderRadius: 20,
+        marginBottom: 30,
+    }
 });
